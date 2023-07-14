a9863368552ffb154cacda615868abf71cbf6404
Rajat Kumar Jha <114743375+rajatdev1@users.noreply.github.com>
2023-07-14 19:17:17 +0530

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,29 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Rajat',
+    lastName: 'jha',
+    email: 'susan@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Harsha',
+    lastName: 'mishra',
+    email: 'adrienne@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Naman',
+    lastName: 'joshi',
+    email: 'rolf@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  }
+ 
+];
+
+export { employeesData };
